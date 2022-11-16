b6c536b289be2c635e8afb7a2ec00a75b93bc371
alexlalov-hub <alex.lalov04@gmail.com>
2022-11-16 15:13:44 +0200

    Removing an unnecessary foreignId in facilities

--- database/migrations/2022_11_15_144324_create_facilities_table.php
+++ database/migrations/2022_11_15_144324_create_facilities_table.php
@@ -15,7 +15,6 @@ return new class extends Migration
     {
         Schema::create('facilities', function (Blueprint $table) {
             $table->id();
-            $table->foreignId("clinic_id")->constrained("clinics")->cascadeOnDelete();
             $table->integer("weekday");
             $table->time("from");
             $table->time("to");

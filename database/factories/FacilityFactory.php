91ae5c04d1731ce58dcb48ab2447750ac32076f4
alexlalov-hub <alex.lalov04@gmail.com>
2022-11-16 15:47:13 +0200

    Deleting unnecessary migration files, updating the users migration and adding a ClinicFactory

--- database/factories/FacilityFactory.php
+++ database/factories/FacilityFactory.php
@@ -17,7 +17,8 @@ class FacilityFactory extends Factory
     public function definition()
     {
         return [
-            //
+            "name" => $this->faker->name,
+            ""
         ];
     }
 }

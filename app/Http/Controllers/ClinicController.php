9a3b56ef9f90c1a30563b0396d0f6ede0183e2a8
alexlalov-hub <alex.lalov04@gmail.com>
2022-11-18 14:02:25 +0200

    Updating the controllers to be resource controllers

--- app/Http/Controllers/ClinicController.php
+++ app/Http/Controllers/ClinicController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class DoctorUserController extends Controller
+class ClinicController extends Controller
 {
     /**
      * Display a listing of the resource.
